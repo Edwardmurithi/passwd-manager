48e8f9047dea8bdaadaa745ad8439a3505389ee9
Edward murithi <murithiedward330@gmail.com>
2024-10-07 15:49:55 +0300

    Fix ValueErro

--- password_manager.py
+++ password_manager.py
@@ -1,15 +1,20 @@
 #!/usr/bin/python3
 import time
 import sys
+from cryptography.fernet import Fernet
+
 
 def prompt_user():
     input("Enter your Master password: ")
 
 
 def view_existing_passwords():
-    with open('account_password.txt', 'r') as file_object:
-        for line in file_object:
-            print(line.rstrip())
+    try:
+        with open('account_password.txt', 'r') as file_object:
+            for line in file_object:
+                print(line.rstrip())
+    except FileNotFoundError:
+        print("File not found!!")
 
 
 def add_new_password():
@@ -21,7 +26,7 @@ def add_new_password():
         account = input(f"\n{'Account Name: ':>20}")
         password = input(f"{'Password: ':>20}")
         with open('account_password.txt', 'a', encoding='utf-8') as file_object:
-            file_object.write(account +  + {password} + '\n')
+            file_object.write(account +  password + '\n')
 
 def pause_and_return():
     input("Press Enter to return to main Menu.")
@@ -34,33 +39,42 @@ def clear_screen():
 
 def quit_application():
     """Quits the application"""
-    print("\nExiting the Application...\n")
+    print("\nExiting the Application...")
     time.sleep(2)
     quit()
 
 def main():
+    clear_screen()
     while True:
-        print(f'\n\n{"WELCOME TO PASSWORD MANAGER":*^50}')
-        print(f"\n{'1. Add a New Password':^50}")
-        print(f"{'2. View Existing Passwords':^54}")
+        print(f'\n{"WELCOME TO PASSWORD MANAGER":*^50}')
+        print(f"\n{'1. Add a new account':^49}")
+        print(f"{'2. View existing accounts':^54}")
         print(f"{'3. Quit':^35}")
 
-        option = int(input(f"\n{'Select an Option: ':>32}"))
-        if option == 1:
+        try:
+            option = int(input(f"{'Select an Option: ':>32}"))
+            if option == 1:
+                clear_screen()
+                add_new_password()
+                pause_and_return()
+            elif option == 2:
+                clear_screen()
+                view_existing_passwords()
+                pause_and_return()
+            elif option == 3:
+                quit_application()
+            else:
+                clear_screen()
+                print("\nInvalid Option!")
+                time.sleep(2)
+                clear_screen()
+                continue
+        except ValueError:
             clear_screen()
-            add_new_password()
-            pause_and_return()
-        elif option == 2:
+            print("\nInvalid value!!")
+            time.sleep(2)
             clear_screen()
-            view_existing_passwords()
-            pause_and_return()
-        elif option == 3:
-            clear_screen()
-            quit_application()
-            pause_and_return()
-        else:
-            print("Invalid Option!")
-            continue
+
     
 
 if __name__ == '__main__':
